f33ffc9535cbe08cb202e02f26550066050e3068
ArturoEspinozaQuintero <arturo.espinoza@base22.com>
2023-01-09 14:13:00 -0600

    Add axios for the request instead of fetch

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-function getAPokemon(pokeName) {
-  let pokeData = null;
-  fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
-    .then((response) => response.json())
-    .then((data) => (pokeData = data))
-    .catch((err) => console.log("An error ocurred: " + err));
+const { default: axios } = require("axios");
 
-  return pokeData;
+async function getAPokemon(pokeName) {
+  let pokeData = null;
+  try {
+    const response = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon/${pokeName}`
+    );
+    const data = response.data;
+    pokeData = data;
+    return data;
+  } catch (error) {
+    console.log("Error on the request: ", error);
+  }
 }
 
 module.exports = getAPokemon;
